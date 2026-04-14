450d3d0d95d2b5c54dffb9ed23027df429b08fdb
Weindel32 <botti.mostro_9l@icloud.com>
2026-04-14 19:43:50 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -721,7 +721,6 @@ doc.text(
     doc.save(`DOUBLEU_ordine_${order.club || "Cliente"}_${fmtITDate(order.updatedAtISO)}.pdf`);
   }
 
-}
 
 function makeProductionPDF(order: Order, mode: PdfMode) {
   const doc = new jsPDF({ unit: "pt", format: "a4" });
